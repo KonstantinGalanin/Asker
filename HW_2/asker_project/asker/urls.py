d2b88a446781c3120ecd0ff617a0512d89f57145
Galanin Konstantin <konstantin.galanin@icloud.com>
2024-03-24 21:26:18 +0300

    added HW_2

---
+++ HW_2/asker_project/asker/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path, include
+from asker import views
+
+urlpatterns = [
+    path('', views.index, name="index"),
+    path('ask/', views.ask, name="ask"),
+    path('hot/', views.hot, name="hot"),
+    path('login/', views.login, name="login"),
+    path('question/<int:question_id>', views.question, name="question"),
+    path('register/', views.register, name="register"),
+    path('settings/', views.settings, name="settings"),
+    path('tag/<str:tag_name>', views.tag, name="tag"),
+]
