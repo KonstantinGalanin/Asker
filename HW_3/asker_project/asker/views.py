7ac67e27fe3845b755ef18cb4a8e3e27379f2650
Galanin Konstantin <konstantin.galanin@icloud.com>
2024-03-30 12:57:20 +0300

    connected postgresql

--- HW_3/asker_project/asker/views.py
+++ HW_3/asker_project/asker/views.py
@@ -72,7 +72,7 @@ def settings(request):
 def tag(request, tag_name):
 
     target_tag = Tag.objects.get(title=tag_name)
-    questions_tag = Question.objects.filter(tags__title=tag_name) # How it works ??
+    questions_tag = Question.objects.filter(tags__title=tag_name)
 
     context = {'questions' : paginate(request, questions_tag),
                'tags' : Tag.objects.popular_tags(),
